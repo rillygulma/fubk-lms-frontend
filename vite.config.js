6e180ceb30aa69b79060f81db67811d5cfb3ced1
Rilwanu Idris <133426348+rillygulma@users.noreply.github.com>
2024-06-12 00:38:26 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,13 @@ export default defineConfig({
     rollupOptions: {
       input: './index.html'
     }
+  },
+  server: {
+    host: true, // Listen on all addresses, including LAN and public IPs
+    port: 3000  // Default port (this can be changed)
+  },
+  preview: {
+    host: true, // Listen on all addresses, including LAN and public IPs
+    port: 5000  // Default port for preview
   }
 });
